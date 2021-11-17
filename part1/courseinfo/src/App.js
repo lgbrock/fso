876b41e8866323091581f4107ec23283402bcc53
Logan Brock <logan.g.brock@gmail.com>
2021-11-17 17:42:07 -0500

    Finished Intro to React - Starting exercise 1.6-1.14

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,69 @@
 import React, { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = (props) => {
+	if (props.allClicks.length === 0) {
+		return <div>the app is used by pressing the buttons</div>;
+	}
+	return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => (
+	<button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-	const [counter, setCounter] = useState(0);
+	const [left, setLeft] = useState(0);
+	const [right, setRight] = useState(0);
+	const [allClicks, setAll] = useState([]);
+	const [value, setValue] = useState(10);
 
-	const increaseByOne = () => setCounter(counter + 1);
-	const decreaseByOne = () => setCounter(counter - 1);
-	const setToZero = () => setCounter(0);
+	const handleLeftClick = () => {
+		setAll(allClicks.concat('L'));
+		setLeft(left + 1);
+	};
+
+	const handleRightClick = () => {
+		setAll(allClicks.concat('R'));
+		setRight(right + 1);
+	};
+
+	const setToValue = (newValue) => {
+		setValue(newValue);
+	};
 
 	return (
 		<div>
-			<Display counter={counter} />
-			<Button onClick={increaseByOne} text='plus' />
-			<Button onClick={setToZero} text='zero' />
-			<Button onClick={decreaseByOne} text='minus' />
+			{left}
+			<Button handleClick={handleLeftClick} text='left' />
+			<Button handleClick={handleRightClick} text='right' />
+			{right}
+			<History allClicks={allClicks} />
+			<Display value={value} />
+			<Button handleClick={() => setValue(1000)} text='thousand' />
+			<Button handleClick={() => setValue(0)} text='reset' />
+			<Button handleClick={() => setValue(value + 1)} text='increment +1' />
 		</div>
 	);
 };
+
+// const App = () => {
+// 	const [counter, setCounter] = useState(0);
+
+// 	const increaseByOne = () => setCounter(counter + 1);
+// 	const decreaseByOne = () => setCounter(counter - 1);
+// 	const setToZero = () => setCounter(0);
+
+// 	return (
+// 		<div>
+// 			<Display counter={counter} />
+// 			<Button onClick={increaseByOne} text='plus' />
+// 			<Button onClick={setToZero} text='zero' />
+// 			<Button onClick={decreaseByOne} text='minus' />
+// 		</div>
+// 	);
+// };
 
 // const Hello = ({ name, age }) => {
 // 	const bornYear = () => new Date().getFullYear() - age;
