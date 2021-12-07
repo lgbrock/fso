17689b87d255ec87a79fa4336db83ec7f0dd3963
Logan Brock <logan.g.brock@gmail.com>
2021-12-06 21:38:11 -0500

    still working on exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,15 +4,37 @@ const Button = (props) => (
 	<button onClick={props.handleClick}>{props.text}</button>
 );
 
-const Display = (props) => {
-	return <div>{props.value}</div>;
+// const Display = (props) => {
+// 	return <div>{props.value}</div>;
+// };
+
+const StatisticLine = (props) => {
+	return (
+		<div>
+			<p>
+				<tr>
+					<td>{props.text}:</td>
+					<td>{props.value}</td>
+				</tr>
+			</p>
+		</div>
+	);
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+	const average = (good - bad) / total;
+	const positive = good / total;
 	return (
 		<div>
 			<h1>Statistics</h1>
-			<Display value={`Good: ${good}`} />
+			<StatisticLine text='Good' value={good} />
+			<StatisticLine text='Neutral' value={neutral} />
+			<StatisticLine text='Bad' value={bad} />
+			<StatisticLine text='All' value={good + neutral + bad} />
+			<StatisticLine text='Average' value={average.toFixed(2) + '%'} />
+			<StatisticLine text='Positive' value={positive.toFixed(2) * 100 + '%'} />
+			{/* <Display value={`Good: ${good}`} />
 			<br />
 			<Display value={`Neutral: ${neutral}`} />
 			<br />
@@ -28,7 +50,7 @@ const Statistics = ({ good, neutral, bad }) => {
 				value={`Positive: ${
 					(good / (good + neutral + bad)).toFixed(2) * 100
 				} %`}
-			/>
+			/> */}
 		</div>
 	);
 };
@@ -71,6 +93,7 @@ const App = () => {
 			<Button handleClick={handleNeutralClick} text='neutral' />
 			<Button handleClick={handleBadClick} text='bad' />
 			<NoFeedback good={good} neutral={neutral} bad={bad} />
+			<StatisticLine />
 		</>
 	);
 };
