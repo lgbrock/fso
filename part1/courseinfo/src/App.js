2f9a64d7fecd992f5716a78b96cefb407246de68
Logan Brock <logan.g.brock@gmail.com>
2021-12-15 13:20:27 -0500

    finished exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,11 +1,85 @@
 import React from 'react';
-import Course from './components/Course';
 
-const App = ({ course }) => {
-	return <Course course={course} />;
+const Header = (props) => {
+	// name of course
+	return (
+		<div>
+			<h1>{props.course}</h1>
+		</div>
+	);
+};
+
+const Content = (props) => {
+	// parts and number of exercises
+	return (
+		<div>
+			<p>
+				{props.parts[0].name} {props.parts[0].exercises}
+			</p>
+			<p>
+				{props.parts[1].name} {props.parts[1].exercises}
+			</p>
+			<p>
+				{props.parts[2].name} {props.parts[2].exercises}
+			</p>
+		</div>
+	);
+};
+
+const Total = (props) => {
+	// total number of exercises
+	return (
+		<div>
+			<p>
+				Number of exercises{' '}
+				{props.parts[0].exercises +
+					props.parts[1].exercises +
+					props.parts[2].exercises}
+			</p>
+		</div>
+	);
+};
+
+const App = () => {
+	const course = 'Half Stack application development';
+	const part1 = 'Fundamentals of React';
+	const exercises1 = 10;
+	const part2 = 'Using props to pass data';
+	const exercises2 = 7;
+	const part3 = 'State of a component';
+	const exercises3 = 14;
+
+	return (
+		<div>
+			<Header course={course} />
+			<Content
+				parts={[
+					{ name: part1, exercises: exercises1 },
+					{ name: part2, exercises: exercises2 },
+					{ name: part3, exercises: exercises3 },
+				]}
+			/>
+			<Total
+				parts={[
+					{ name: part1, exercises: exercises1 },
+					{ name: part2, exercises: exercises2 },
+					{ name: part3, exercises: exercises3 },
+				]}
+			/>
+		</div>
+	);
 };
 
 export default App;
+
+// import React from 'react';
+// import Course from './components/Course';
+
+// const App = ({ course }) => {
+// 	return <Course course={course} />;
+// };
+
+// export default App;
 
 // const History = (props) => {
 // 	if (props.allClicks.length === 0) {
