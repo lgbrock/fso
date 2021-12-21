45724cd38b3c50c5f40c4f006bbd29f35c51eaa5
Logan Brock <logan.g.brock@gmail.com>
2021-12-21 18:25:26 -0500

    cound not figure out how to render countries on page with api

---
+++ part2/countries/src/App.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+	return (
+		<div>
+			find countries <input />
+		</div>
+	);
+};
+
+export default App;
