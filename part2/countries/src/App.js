3e7c7acd55337f221fe77eab4660f12791a9b82d
Logan Brock <logan.g.brock@gmail.com>
2021-12-21 18:53:36 -0500

    back to sqaure 1 on exercise 2.12 - moving onto next section

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,6 +2,16 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
+	const [searchName, setSearchName] = useState('');
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		axios.get('https://restcountries.com/v3.1/all').then((response) => {
+			setCountries(response.data);
+		});
+	}, []);
+	console.log(countries);
+
 	return (
 		<div>
 			find countries <input />
