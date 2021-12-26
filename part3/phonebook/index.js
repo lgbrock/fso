d2b906283d6363bf2edfd3ab2518cbbf71087e1c
Logan Brock <logan.g.brock@gmail.com>
2021-12-26 10:43:06 -0500

    finished exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -48,6 +48,18 @@ app.get('/api/persons/:id', (request, response) => {
 	}
 });
 
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((person) => person.id === id);
+
+	if (person) {
+		persons = persons.filter((person) => person.id !== id);
+		res.status(204).end();
+	} else {
+		res.status(404).end();
+	}
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
