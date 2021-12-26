f19d5fff28d1a048e42ff8befb2c5984554855b4
Logan Brock <logan.g.brock@gmail.com>
2021-12-26 18:35:12 -0500

    created new folder for practice - note examples

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -4,29 +4,3 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// axios.get('http://localhost:3001/notes').then((response) => {
-// 	const notes = response.data;
-// 	console.log(notes);
-// });
-
-// *-- Unpractical Usages --*
-// axios.get('http://localhost:3001/notes').then((response) => {
-// 	const notes = response.data;
-// 	console.log(notes);
-// });
-
-// const promise = axios.get('http://localhost:3001/notes');
-// promise.then((response) => {
-// 	console.log(response);
-// });
-
-// const promise2 = axios.get('http://localhost:3001/foobar');
-// console.log(promise2);
-
-// ReactDOM.render(
-// 	<React.StrictMode>
-// 		<App />
-// 	</React.StrictMode>,
-// 	document.getElementById('root')
-// );
--- quokka/arrayRest.js
+++
@@ -1,5 +0,0 @@
-const t = [1, 2, 3, 4, 5];
-
-const [first, second, ...rest] = t;
-
-console.log(first, second, ...rest);
