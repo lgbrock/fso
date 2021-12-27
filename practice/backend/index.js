b6d9fe7f81f97b37caf718254985360317073170
Logan Brock <logan.g.brock@gmail.com>
2021-12-27 14:38:24 -0500

    finished practice - deploying app to internet - starting exercise 3.9

--- practice/backend/index.js
+++ practice/backend/index.js
@@ -3,11 +3,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+// middleware
+app.use(express.static('build'));
+app.use(cors());
 app.use(express.json());
 
-// middleware
-app.use(cors());
-app.use(express.static('build'));
+// Displays contents of notes in terminal - more secure
+const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method);
+	console.log('Path:  ', request.path);
+	console.log('Body:  ', request.body);
+	console.log('---');
+	next();
+};
+
+app.use(requestLogger);
 
 let notes = [
 	{
@@ -38,17 +48,6 @@ app.get('/api/notes', (request, response) => {
 	response.json(notes);
 });
 
-app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
-		response.json(note);
-	} else {
-		response.status(404).end();
-	}
-});
-
 const generateId = () => {
 	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
 	return maxId + 1;
@@ -75,12 +74,30 @@ app.post('/api/notes', (request, response) => {
 	response.json(note);
 });
 
+app.get('/api/notes/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.find((note) => note.id === id);
+
+	if (note) {
+		response.json(note);
+	} else {
+		response.status(404).end();
+	}
+});
+
 app.delete('/api/notes/:id', (request, response) => {
 	const id = Number(request.params.id);
 	notes = notes.filter((note) => note.id !== id);
 
 	response.status(204).end();
 });
+
+// Displays error message in window if endpoint is not found
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
