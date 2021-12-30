c1b24a04add75ffceba83b22d5805cf366fee3fe
Logan Brock <logan.g.brock@gmail.com>
2021-12-30 10:42:45 -0500

    finished exercise 4.2

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,16 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+// GET all blogs
+blogsRouter.get('/', (request, response) => {
+	Blog.find({}).then((blogs) => response.json(blogs));
+});
+
+// POST/create blog post
+blogsRouter.post('/', (request, response) => {
+	const blog = new Blog(request.body);
+
+	blog.save().then((result) => response.status(201).json(result));
+});
+
+module.exports = blogsRouter;
