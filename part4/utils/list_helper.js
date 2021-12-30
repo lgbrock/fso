eed15b93470cb3ef809ef644e438bd265d634de9
Logan Brock <logan.g.brock@gmail.com>
2021-12-30 12:34:39 -0500

    finished exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,12 @@ const dummy = (blogs) => {
 	return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+	const reducer = (sum, blog) => {
+		return sum + blog.likes;
+	};
+
+	return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
+module.exports = { dummy, totalLikes };
