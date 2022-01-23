976443a29ef90b0f92da6bbe3aff3eddc2aba889
Logan Brock <logan.g.brock@gmail.com>
2022-01-23 16:28:36 -0500

    finished practice - starting exercises 5.1-5.4

--- practice/frontend/src/App.js
+++ practice/frontend/src/App.js
@@ -22,6 +22,15 @@ const App = () => {
 		});
 	}, []);
 
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			setUser(user);
+			noteService.setToken(user.token);
+		}
+	}, []);
+
 	const addNote = (event) => {
 		event.preventDefault();
 		const noteObject = {
@@ -69,6 +78,7 @@ const App = () => {
 				username,
 				password,
 			});
+			window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
 			noteService.setToken(user.token);
 			setUser(user);
 			setUsername('');
