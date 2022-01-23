32da8f30d4bc868f61dae8c146e619c2e082d3ae
Logan Brock <logan.g.brock@gmail.com>
2022-01-22 19:34:22 -0500

    quick update

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,9 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
 
-export default Blog
+const Blog = ({ blog }) => (
+	<div>
+		{blog.title} {blog.author}
+	</div>
+);
+
+export default Blog;
