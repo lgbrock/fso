b9d65e9d57e955d1ece2af35ce214d8a47f2f96f
Logan Brock <logan.g.brock@gmail.com>
2022-01-25 21:43:00 -0500

    Starting exercises 5.13-5.16

--- practice/frontend/src/components/test/Note.test.js
+++ practice/frontend/src/components/test/Note.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { prettyDOM } from '@testing-library/react';
 import Note from '../Note';
 
@@ -32,4 +32,20 @@ test('renders content', () => {
 	expect(div).toHaveTextContent(
 		'Component testing is done with react-testing-library'
 	);
+});
+
+test('clicking the button calls event handler once', () => {
+	const note = {
+		content: 'Component testing is done with react-testing-library',
+		important: true,
+	};
+
+	const mockHandler = jest.fn();
+
+	const component = render(<Note note={note} toggleImportance={mockHandler} />);
+
+	const button = component.getByText('make not important');
+	fireEvent.click(button);
+
+	expect(mockHandler.mock.calls).toHaveLength(1);
 });
