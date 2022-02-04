44261c12e3529ea4bb63752e56cbc59a23e7000d
Logan Brock <logan.g.brock@gmail.com>
2022-02-03 20:35:46 -0500

    starting exercises 6.19-6.21

--- part6/redux-practice/src/components/NewNote.jsx
+++ part6/redux-practice/src/components/NewNote.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createNote } from '../reducers/noteReducer';
 
 const NewNote = (props) => {
-	const addNote = async (event) => {
+	const addNote = (event) => {
 		event.preventDefault();
 		const content = event.target.note.value;
 		event.target.note.value = '';
@@ -18,4 +18,12 @@ const NewNote = (props) => {
 	);
 };
 
-export default connect(null, { createNote })(NewNote);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		createNote: (value) => {
+			dispatch(createNote(value));
+		},
+	};
+};
+
+export default connect(null, { mapDispatchToProps })(NewNote);
