8a837962457cebb8a4d231d455bc22722f89a953
Logan Brock <logan.g.brock@gmail.com>
2022-02-06 13:59:54 -0500

    finished exercise 7.1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,137 +1,188 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
-  return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
-  )
-}
+	const padding = {
+		paddingRight: 10,
+	};
+	return (
+		<div>
+			<a href='/' style={padding}>
+				anecdotes
+			</a>
+			<a href='/create' style={padding}>
+				create new
+			</a>
+			<a href='about' style={padding}>
+				about
+			</a>
+		</div>
+	);
+};
 
 const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>
-  </div>
-)
+	<div>
+		<h2>Anecdotes</h2>
+		<ul>
+			{anecdotes.map((anecdote) => (
+				<li key={anecdote.id}>{anecdote.content}</li>
+			))}
+		</ul>
+	</div>
+);
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+	<div>
+		<h2>About anecdote app</h2>
+		<p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+		<em>
+			An anecdote is a brief, revealing account of an individual person or an
+			incident. Occasionally humorous, anecdotes differ from jokes because their
+			primary purpose is not simply to provoke laughter but to reveal a truth
+			more general than the brief tale itself, such as to characterize a person
+			by delineating a specific quirk or trait, to communicate an abstract idea
+			about a person, place, or thing through the concrete details of a short
+			narrative. An anecdote is "a story with a point."
+		</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
+		<p>
+			Software engineering is full of excellent anecdotes, at this app you can
+			find the best and add more.
+		</p>
+	</div>
+);
 
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
+const Footer = () => {
+	const padding = {
+		paddingTop: 20,
+	};
 
-    See <a href='https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
-  </div>
-)
+	return (
+		<div style={padding}>
+			Anecdote app for{' '}
+			<a href='https://courses.helsinki.fi/fi/tkt21009'>
+				Full Stack -websovelluskehitys
+			</a>
+			. See{' '}
+			<a href='https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js'>
+				https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js
+			</a>{' '}
+			for the source code.
+		</div>
+	);
+};
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+	const [content, setContent] = useState('');
+	const [author, setAuthor] = useState('');
+	const [info, setInfo] = useState('');
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		props.addNew({
+			content,
+			author,
+			info,
+			votes: 0,
+		});
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    props.addNew({
-      content,
-      author,
-      info,
-      votes: 0
-    })
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-
-}
+	return (
+		<div>
+			<h2>create a new anecdote</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					content
+					<input
+						name='content'
+						value={content}
+						onChange={(e) => setContent(e.target.value)}
+					/>
+				</div>
+				<div>
+					author
+					<input
+						name='author'
+						value={author}
+						onChange={(e) => setAuthor(e.target.value)}
+					/>
+				</div>
+				<div>
+					url for more info
+					<input
+						name='info'
+						value={info}
+						onChange={(e) => setInfo(e.target.value)}
+					/>
+				</div>
+				<button>create</button>
+			</form>
+		</div>
+	);
+};
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: '1'
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: '2'
-    }
-  ])
+	const padding = {
+		paddingTop: 25,
+		marginLeft: 15,
+	};
 
-  const [notification, setNotification] = useState('')
+	const [anecdotes, setAnecdotes] = useState([
+		{
+			content: 'If it hurts, do it more often',
+			author: 'Jez Humble',
+			info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+			votes: 0,
+			id: '1',
+		},
+		{
+			content: 'Premature optimization is the root of all evil',
+			author: 'Donald Knuth',
+			info: 'http://wiki.c2.com/?PrematureOptimization',
+			votes: 0,
+			id: '2',
+		},
+	]);
 
-  const addNew = (anecdote) => {
-    anecdote.id = (Math.random() * 10000).toFixed(0)
-    setAnecdotes(anecdotes.concat(anecdote))
-  }
+	const [notification, setNotification] = useState('');
 
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
+	const addNew = (anecdote) => {
+		anecdote.id = (Math.random() * 10000).toFixed(0);
+		setAnecdotes(anecdotes.concat(anecdote));
+	};
 
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
+	const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
 
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
+	const vote = (id) => {
+		const anecdote = anecdoteById(id);
 
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
-  }
+		const voted = {
+			...anecdote,
+			votes: anecdote.votes + 1,
+		};
 
-  return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
-  )
-}
+		setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+	};
+
+	return (
+		<div style={padding}>
+			<h1>Software anecdotes</h1>
+			<Menu />
+			<Switch>
+				<Route path='/about'>
+					<About />
+				</Route>
+				<Route path='/create'>
+					<CreateNew addNew={addNew} />
+				</Route>
+				<Route path='/'>
+					<AnecdoteList anecdotes={anecdotes} />
+				</Route>
+			</Switch>
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
