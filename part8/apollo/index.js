b9f8e2a51bc963332706ff6d7c23439c2e2a99f7
Logan Brock <logan.g.brock@gmail.com>
2022-02-10 21:43:14 -0500

    finished exercise 8.1

--- part8/apollo/index.js
+++ part8/apollo/index.js
@@ -1,4 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, UserInputError, gql } = require('apollo-server');
+
+const uuid = require('uuid/v1');
 
 let persons = [
 	{
@@ -27,23 +29,77 @@ const typeDefs = gql`
 	type Person {
 		name: String!
 		phone: String
-		street: String!
-		city: String!
+		address: Address!
 		id: ID!
 	}
-
+	type Address {
+		street: String!
+		city: String!
+	}
+	enum YesNo {
+		YES
+		NO
+	}
 	type Query {
 		personCount: Int!
-		allPersons: [Person!]!
+		allPersons(phone: YesNo): [Person!]!
 		findPerson(name: String!): Person
+	}
+	type Mutation {
+		addPerson(
+			name: String!
+			phone: String
+			street: String!
+			city: String!
+		): Person
+		editNumber(name: String!, phone: String!): Person
 	}
 `;
 
 const resolvers = {
 	Query: {
 		personCount: () => persons.length,
-		allPersons: () => persons,
+		allPersons: (root, args) => {
+			if (!args.phone) {
+				return persons;
+			}
+
+			const byPhone = (person) =>
+				args.phone === 'YES' ? person.phone : !person.phone;
+
+			return persons.filter(byPhone);
+		},
 		findPerson: (root, args) => persons.find((p) => p.name === args.name),
+	},
+	Person: {
+		address: (root) => {
+			return {
+				street: root.street,
+				city: root.city,
+			};
+		},
+	},
+	Mutation: {
+		addPerson: (root, args) => {
+			if (persons.find((p) => p.name === args.name)) {
+				throw new UserInputError('Name must be unique', {
+					invalidArgs: args.name,
+				});
+			}
+			const person = { ...args, id: uuid() };
+			persons = persons.concat(person);
+			return person;
+		},
+		editNumber: (root, args) => {
+			const person = persons.find((p) => p.name === args.name);
+			if (!person) {
+				return null;
+			}
+
+			const updatedPerson = { ...person, phone: args.phone };
+			persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+			return updatedPerson;
+		},
 	},
 };
 
