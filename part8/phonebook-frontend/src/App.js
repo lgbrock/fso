4a5128cea0d5f963f1bcbcd20fef667666cc4734
Logan Brock <logan.g.brock@gmail.com>
2022-02-22 19:59:32 -0500

    8.b cache

--- part8/phonebook-frontend/src/App.js
+++ part8/phonebook-frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 const ALL_PERSONS = gql`
@@ -10,13 +11,58 @@ const ALL_PERSONS = gql`
 	}
 `;
 
+const FIND_PERSON = gql`
+	query findPersonByName($nameToSearch: String!) {
+		findPerson(name: $nameToSearch) {
+			name
+			phone
+			id
+			address {
+				street
+				city
+			}
+		}
+	}
+`;
+
+const Person = ({ person, onClose }) => {
+	return (
+		<div>
+			<h2>{person.name}</h2>
+			<div>
+				{person.address.street} {person.address.city}
+			</div>
+			<div>
+				<p>{person.phone}</p>
+			</div>
+			<button onClick={onClose}>close</button>
+		</div>
+	);
+};
+
 const Persons = ({ persons }) => {
+	const [nameToSearch, setNameToSearch] = useState(null);
+	const result = useQuery(FIND_PERSON, {
+		variables: { nameToSearch },
+		skip: !nameToSearch,
+	});
+
+	if (nameToSearch && result.data) {
+		return (
+			<Person
+				person={result.data.findPerson}
+				onClose={() => setNameToSearch(null)}
+			/>
+		);
+	}
+
 	return (
 		<div>
 			<h2>Persons</h2>
 			{persons.map((p) => (
 				<div key={p.name}>
 					{p.name} {p.phone}
+					<button onClick={() => setNameToSearch(p.name)}>show address</button>
 				</div>
 			))}
 		</div>
