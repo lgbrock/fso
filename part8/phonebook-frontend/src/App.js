e23d0a3c803fc141d3f318f65d19df3d97ed8200
Logan Brock <logan.g.brock@gmail.com>
2022-02-23 11:31:51 -0500

    8.b updating the cache

--- part8/phonebook-frontend/src/App.js
+++ part8/phonebook-frontend/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 
 const ALL_PERSONS = gql`
 	query {
@@ -11,72 +12,21 @@ const ALL_PERSONS = gql`
 	}
 `;
 
-const FIND_PERSON = gql`
-	query findPersonByName($nameToSearch: String!) {
-		findPerson(name: $nameToSearch) {
-			name
-			phone
-			id
-			address {
-				street
-				city
-			}
-		}
-	}
-`;
-
-const Person = ({ person, onClose }) => {
-	return (
-		<div>
-			<h2>{person.name}</h2>
-			<div>
-				{person.address.street} {person.address.city}
-			</div>
-			<div>
-				<p>{person.phone}</p>
-			</div>
-			<button onClick={onClose}>close</button>
-		</div>
-	);
-};
-
-const Persons = ({ persons }) => {
-	const [nameToSearch, setNameToSearch] = useState(null);
-	const result = useQuery(FIND_PERSON, {
-		variables: { nameToSearch },
-		skip: !nameToSearch,
-	});
-
-	if (nameToSearch && result.data) {
-		return (
-			<Person
-				person={result.data.findPerson}
-				onClose={() => setNameToSearch(null)}
-			/>
-		);
-	}
-
-	return (
-		<div>
-			<h2>Persons</h2>
-			{persons.map((p) => (
-				<div key={p.name}>
-					{p.name} {p.phone}
-					<button onClick={() => setNameToSearch(p.name)}>show address</button>
-				</div>
-			))}
-		</div>
-	);
-};
-
 const App = () => {
-	const result = useQuery(ALL_PERSONS);
+	const result = useQuery(ALL_PERSONS, {
+		pollInterval: 2000,
+	});
 
 	if (result.loading) {
 		return <div>loading...</div>;
 	}
 
-	return <Persons persons={result.data.allPersons} />;
+	return (
+		<div>
+			<Persons persons={result.data.allPersons} />
+			<PersonForm />
+		</div>
+	);
 };
 
 export default App;
