d86c779593495809eef053cdb300bb1c3d389046
Logan Brock <logan.g.brock@gmail.com>
2022-03-03 12:12:24 -0500

    working through exercise 8.13

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,90 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const Author = require('./models/author');
+const Book = require('./models/book');
+
+const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const { v1: uuid } = require('uuid');
 
-let authors = [
-	{
-		name: 'Robert Martin',
-		id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-		born: 1952,
-	},
-	{
-		name: 'Martin Fowler',
-		id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-		born: 1963,
-	},
-	{
-		name: 'Fyodor Dostoevsky',
-		id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-		born: 1821,
-	},
-	{
-		name: 'Joshua Kerievsky', // birthyear not known
-		id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-	},
-	{
-		name: 'Sandi Metz', // birthyear not known
-		id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-	},
-];
+const JWT_SECRET = 'secret';
 
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
+let authors = [];
 
-let books = [
-	{
-		title: 'Clean Code',
-		published: 2008,
-		author: 'Robert Martin',
-		id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring'],
-	},
-	{
-		title: 'Agile software development',
-		published: 2002,
-		author: 'Robert Martin',
-		id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-		genres: ['agile', 'patterns', 'design'],
-	},
-	{
-		title: 'Refactoring, edition 2',
-		published: 2018,
-		author: 'Martin Fowler',
-		id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring'],
-	},
-	{
-		title: 'Refactoring to patterns',
-		published: 2008,
-		author: 'Joshua Kerievsky',
-		id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring', 'patterns'],
-	},
-	{
-		title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-		published: 2012,
-		author: 'Sandi Metz',
-		id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-		genres: ['refactoring', 'design'],
-	},
-	{
-		title: 'Crime and punishment',
-		published: 1866,
-		author: 'Fyodor Dostoevsky',
-		id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-		genres: ['classic', 'crime'],
-	},
-	{
-		title: 'The Demon ',
-		published: 1872,
-		author: 'Fyodor Dostoevsky',
-		id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-		genres: ['classic', 'revolution'],
-	},
-];
+let books = [];
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/library';
+
+console.log('connecting to', MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+
+mongoose
+	.connect(MONGODB_URI)
+	.then(() => {
+		console.log('connected to MongoDB');
+	})
+	.catch((error) => {
+		console.log('error connection to MongoDB:', error.message);
+	});
 
 const typeDefs = gql`
 	type Author {
@@ -123,7 +69,7 @@ const typeDefs = gql`
 
 const resolvers = {
 	Query: {
-		authorCount: () => authors.length,
+		authorCount: async () => Author.collection.countDocuments(),
 		allBooks: (root, args) => {
 			if (!args.author && !args.genre) {
 				return books;
