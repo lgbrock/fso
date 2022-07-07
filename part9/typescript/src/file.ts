a4014228d0d0f5123730cc6d4430e747523f73f0
Logan Brock <logan.g.brock@gmail.com>
2022-07-07 12:24:29 -0400

    starting improving the project

--- part9/typescript/src/file.ts
+++ part9/typescript/src/file.ts
@@ -1,2 +1,19 @@
-const anExampleVariable = 'Hello World';
-console.log(anExampleVariable);
+type Operation = 'multiply' | 'add' | 'divide';
+
+type Result = number;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+	switch (op) {
+		case 'multiply':
+			return a * b;
+		case 'divide':
+			if (b === 0) throw new Error("Can't divide by 0!");
+			return a / b;
+		case 'add':
+			return a + b;
+		default:
+			throw new Error('Operation is not multiply, add or divide!');
+	}
+};
+
+console.log(process.argv);
